3ae0f255de82ed634685f378fc7c267e6f01d8de
caleskog <christoffer.aleskog@gmail.com>
2024-07-05 00:19:08 +0200

    refactored plugins and added new mini.ai keymapps to which-key

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,90 @@
+-- File: plugins/mini.lua
+-- Author: caleskog
+-- Description: Usefull plugins/modules for improving overall Neovim experience.
+local util = require('../util')
+
+local function mini_ai()
+    -- Better Around/Inside textobjects
+    --
+    -- Examples:
+    --  - va)  - [V]isually select [A]round [)]paren
+    --  - yinq - [Y]ank [I]nside [N]ext [']quote
+    --  - ci'  - [C]hange [I]nside [']quote
+    local setup_tlb = {
+        n_lines = 500,
+    }
+    if util.module_mxists('nvim-treesitter-textobjects') then
+        local spec_treesitter = require('mini.ai').gen_spec.treesitter
+        setup_tlb['custom_textobjects'] = {
+            a = spec_treesitter({ a = '@parameter.outer', i = '@parameter.inner' }),
+            f = spec_treesitter({ a = '@call.outer', i = '@call.inner' }),
+            F = spec_treesitter({ a = '@function.outer', i = '@function.inner' }),
+            c = spec_treesitter({ a = '@conditional.outer', i = '@conditional.inner' }),
+            C = spec_treesitter({ a = '@class.outer', i = '@class.inner' }),
+            S = spec_treesitter({ a = '@statement.outer', i = '@statement.inner' }),
+            L = spec_treesitter({ a = '@loop.outer', i = '@loop.inner' }),
+            -- b = spec_treesitter({ a = '@block.outer', i = '@block.inner' }),
+        }
+    end
+
+    require('mini.ai').setup(setup_tlb)
+
+    local desc_t = {
+        util.WhichKey_ai('[a]rgument/parameter'),
+        util.WhichKey_ai('[f]unction call'),
+        util.WhichKey_ai('[F]unction'),
+        util.WhichKey_ai('[c]onditional'),
+        util.WhichKey_ai('[C]lass'),
+        util.WhichKey_ai('[S]tatement'),
+        util.WhichKey_ai('[L]oop'),
+        -- util.WhichKey_ai('[b]lock'),    --TODO: 'ab' and 'ib' doeen't whow up in which-key. I don't know if it uses the treesitter version or not?
+
+        -- The following didn't work due to errors in which-key on start-up
+        -- B = false, -- Disabling aB and iB as treesitter is a better option for block searches
+    }
+    -- util.dump_dict(desc_t, 'desc_t_before.dump')
+    desc_t = util.flatten(desc_t)
+    -- util.dump_dict(desc_t, 'desc_t_after.dump')
+
+    -- Document Mini.AI custom textobjects for operator-pending mode
+    require('which-key').register(desc_t, { mode = 'o', prefix = '' })
+    -- Document Mini.AI custom textobjects for visual mode
+    require('which-key').register(desc_t, { mode = 'x', prefix = '' })
+end
+
+return {
+    { -- Collection of various small independent plugins/modules
+        'echasnovski/mini.nvim',
+        version = '*',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        },
+        config = function()
+            mini_ai()
+            -- Add/delete/replace surroundings (brackets, quotes, etc.)
+            --
+            -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+            -- - sd'   - [S]urround [D]elete [']quotes
+            -- - sr)'  - [S]urround [R]eplace [)] [']
+            require('mini.surround').setup()
+
+            -- Simple and easy statusline.
+            --  You could remove this setup call if you don't like it,
+            --  and try some other statusline plugin
+            local statusline = require('mini.statusline')
+            -- set use_icons to true if you have a Nerd Font
+            statusline.setup({ use_icons = vim.g.have_nerd_font })
+
+            -- You can configure sections in the statusline by overriding their
+            -- default behavior. For example, here we set the section for
+            -- cursor location to LINE:COLUMN
+            ---@diagnostic disable-next-line: duplicate-set-field
+            statusline.section_location = function()
+                return '%2l:%-2v'
+            end
+
+            -- ... and there is more!
+            --  Check out: https://github.com/echasnovski/mini.nvim
+        end,
+    },
+}
---
+++ lua/util.lua
@@ -0,0 +1,85 @@
+-- File: util.lua
+-- Author: caleskog
+-- Description: Utility functions.
+
+local M = {}
+
+-- Check if a module exists before requiring it
+-- @param name - module name
+function M.module_mxists(name)
+    local status, _ = pcall(require, name)
+    return status
+end
+
+---Generate a which-key register table for additional around/inside functionality.
+---
+---The following is an example of what it would produce:
+---     M.WhichKey_ai("[A]rgument")
+--- would produce the following which-key register table:
+---     {
+---         ["aA"] = "[a]round [A]rgument",
+---         ["iA"] = "[i]nside [A]rgument",
+---     }
+---
+---Note: The function `M.flatten` is needed if the function will be used more than once.
+---
+---@param identifier string Identifier name including the textobject id surrounded by [].
+---@return table
+function M.WhichKey_ai(identifier)
+    local i, j = string.find(identifier, '%[.%]')
+    if i == nil then
+        return {}
+    end
+    local key = string.sub(identifier, i + 1, j - 1)
+    return {
+        ['a' .. key] = '[a]round ' .. identifier,
+        ['i' .. key] = '[i]inside ' .. identifier,
+    }
+end
+
+---Dump the content of a dictionary into a new file.
+---
+---@param dict table Dictionary to be written to file.
+---@param filename? string Name of the dump file. Deafult: `'dict.dump'`
+function M.dump_dict(dict, filename)
+    filename = filename or 'dict.dump'
+    local content = vim.inspect(dict)
+    local file = io.open(filename, 'w')
+    if not file then
+        error('Could not open file ' .. filename .. 'for writing')
+    end
+    file:write(content)
+    file:close()
+end
+
+---Flatten a dictionary of keys of type string (`[<string>]`).
+---
+---@param tlb table The table to be flattend
+---@param depth? integer The depth to be considered. Default: `-1`
+---@param parent_key? string Key from the parent table. Default: `""`
+---@return table # The flattened table
+function M.flatten(tlb, depth, parent_key)
+    local flat_table = {}
+    depth = depth or -1
+    parent_key = parent_key or ''
+
+    local function _flatten(t, current_depth, current_key)
+        for k, v in pairs(t) do
+            local new_key = current_key
+            if type(k) ~= 'number' then
+                new_key = current_key .. (current_key ~= '' and '.' or '') .. k
+            end
+
+            if type(v) == 'table' and current_depth ~= depth then
+                _flatten(v, depth + 1, new_key)
+            else
+                flat_table[new_key] = v
+            end
+        end
+    end
+
+    _flatten(tlb, depth + 1, parent_key)
+    return flat_table
+end
+
+return M
