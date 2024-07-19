1d720c27ea82a89b7d86ce4dd6776dd6154c8999
caleskog <christoffer.aleskog@gmail.com>
2024-07-19 11:29:00 +0200

    Mapping for open files via system default in neotree

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -26,7 +26,15 @@ return {
                     ['<C-d>'] = { 'scroll_preview', config = { direction = -10 } },
                     ['<C-b>'] = 'noop',
                     ['<C-f>'] = 'noop',
+                    ['O'] = 'system_open',
                 },
+            },
+            commands = {
+                system_open = function(state)
+                    local node = state.tree:get_node()
+                    local path = node:get_id()
+                    vim.fn.jobstart({ 'xdg-open', path }, { detach = true })
+                end,
             },
         })
     end,
