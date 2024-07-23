ea21fd4807679101c424a38d668ab23cfc5b56fa
caleskog <christoffer.aleskog@gmail.com>
2024-07-23 16:47:59 +0200

    Added pre and post formatters for clang-format to indent #pragma's

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -261,11 +261,74 @@ return {
             local conform = require('conform')
             conform.setup({
                 notify_on_error = false,
+                formatters = {
+                    ['clang-fmt-pre'] = {
+                        ---Replace all `#pragma omp ...` with `//#pragma omp ...`
+                        format = function(self, ctx, lines, callback)
+                            -- Use this variable if options should be possible
+                            local _ = self.options
+                            local format_erros = nil
+                            local formatted_lines = vim.deepcopy(lines)
+                            local pattern = '^%s*#pragma omp'
+                            -- If a range is provided, only format that range
+                            if ctx.range then
+                                local row = ctx.range.start[1]
+                                local end_row = ctx.range['end'][1]
+                                for i = row, end_row do
+                                    local line = lines[i]
+                                    if line:match(pattern) then
+                                        formatted_lines[i] = line:gsub(pattern, '//#pragma omp')
+                                    end
+                                end
+                            else
+                                for i, line in ipairs(lines) do
+                                    if line:match(pattern) then
+                                        -- vim.notify('ORG_LINE: »' .. line .. '«')
+                                        local fmt_line = line:gsub(pattern, '//#pragma omp')
+                                        -- vim.notify('FMT_LINE: »' .. fmt_line .. '«')
+                                        formatted_lines[i] = fmt_line
+                                    end
+                                end
+                            end
+                            callback(format_erros, formatted_lines)
+                        end,
+                    },
+                    ['clang-fmt-post'] = {
+                        ---Replace all `//#pragma omp ...` with `#pragma omp ...`
+                        format = function(self, ctx, lines, callback)
+                            -- Use this variable if options should be possible
+                            local _ = self.options
+                            local format_erros = nil
+                            local formatted_lines = vim.deepcopy(lines)
+                            local pattern = '//%s#pragma omp'
+                            -- If a range is provided, only format that range
+                            if ctx.range then
+                                local row = ctx.range.start[1]
+                                local end_row = ctx.range['end'][1]
+                                -- local col = ctx.range.start[2]
+                                -- local end_col = ctx.range['end'][2]
+                                for i = row, end_row do
+                                    local line = lines[i]
+                                    if line:match(pattern) then
+                                        formatted_lines[i] = line:gsub(pattern, '#pragma omp')
+                                    end
+                                end
+                            else
+                                for i, line in ipairs(formatted_lines) do
+                                    if line:match(pattern) then
+                                        formatted_lines[i] = line:gsub(pattern, '#pragma omp')
+                                    end
+                                end
+                            end
+                            callback(format_erros, formatted_lines)
+                        end,
+                    },
+                },
                 formatters_by_ft = {
                     lua = { 'stylua' },
                     bashls = { 'shfmt' },
                     c = { 'clang-format' },
-                    cpp = { 'clang-format', 'cppcheck' },
+                    cpp = { 'cppcheck', 'clang-fmt-pre', 'clang-format', 'clang-fmt-post' },
                     asm = { 'asmfmt' },
                     cmake = { 'cmakelang' },
                     -- Conform can also run multiple formatters sequentially
@@ -286,6 +349,9 @@ return {
                     if bufname:match('premake5.lua$') then
                         return
                     end
+                    -- Preprocess the code if it's a C/C++ file
+                    -- if vim.bo[bufnr].filetype == 'c' or vim.bo[bufnr].filetype == 'cpp' then
+                    -- end
                     return {
                         timeout_ms = 500,
                         lsp_fallback = 'fallback',
