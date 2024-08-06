0c6b0c76f83b38d6c9b4921ff476b42a90eddc4c
caleskog <christoffer.aleskog@gmail.com>
2024-08-06 17:21:58 +0200

    Allow to use arrow keys to move between tmux and neovim

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,10 +1,39 @@
 -- File: navigation.lua
 -- Author: caleskog
 
+local util = require('util')
+
 return {
     {
         'aserowy/tmux.nvim',
         config = function()
+            -- Allow arrow keys to move between panes
+            util.fmap('n', '<C-Left>', function()
+                require('tmux').move_left()
+            end, 'Move to the left')
+            util.fmap('n', '<C-Down>', function()
+                require('tmux').move_bottom()
+            end, 'Move to the bottom', { noremap = true })
+            util.fmap('n', '<C-Up>', function()
+                require('tmux').move_top()
+            end, 'Move to the top', { noremap = true })
+            util.fmap('n', '<C-Right>', function()
+                require('tmux').move_right()
+            end, 'Move to the right')
+
+            util.fmap('n', '<A-Left>', function()
+                require('tmux').resize_left()
+            end, 'Resize to the left')
+            util.fmap('n', '<A-Down>', function()
+                require('tmux').resize_bottom()
+            end, 'Resize to the bottom')
+            util.fmap('n', '<A-Up>', function()
+                require('tmux').resize_top()
+            end, 'Resize to the top')
+            util.fmap('n', '<A-Right>', function()
+                require('tmux').resize_right()
+            end, 'Resize to the right')
+
             return require('tmux').setup({
                 copy_sync = {
                     -- enables copy sync. by default, all registers are synchronized.
@@ -45,14 +74,14 @@ return {
                     cycle_navigation = true,
 
                     -- enables default keybindings (C-hjkl) for normal mode
-                    enable_default_keybindings = true,
+                    enable_default_keybindings = false,
 
                     -- prevents unzoom tmux when navigating beyond vim border
                     persist_zoom = false,
                 },
                 resize = {
                     -- enables default keybindings (A-hjkl) for normal mode
-                    enable_default_keybindings = true,
+                    enable_default_keybindings = false,
 
                     -- sets resize steps for x axis
                     resize_step_x = 5,
