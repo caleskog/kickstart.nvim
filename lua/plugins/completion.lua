2810020303cb51a891022cbcc7dd722069be2cc4
caleskog <christoffer.aleskog@gmail.com>
2024-09-15 00:15:33 +0200

    Refactored completions

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -16,62 +16,54 @@ return {
         end,
     },
     {
+        -- nvim-cmp source for Copilot
         'zbirenbaum/copilot-cmp',
+        dependencies = {
+            'zbirenbaum/copilot.lua',
+        },
         config = function()
             require('copilot_cmp').setup()
+        end,
+    },
+    {
+        -- Snippet Engine
+        'L3MON4D3/LuaSnip',
+        dependencies = {
+            -- Premade snippets for many languages (https://github.com/rafamadriz/friendly-snippets)
+            'rafamadriz/friendly-snippets',
+        },
+    },
+    {
+        -- VSCode-like pictograms
+        'onsails/lspkind.nvim',
+        config = function()
+            local lspkind = require('lspkind')
+            lspkind.init({
+                symbol_map = {
+                    Copilot = '', -- Not default
+                },
+            })
+            vim.api.nvim_set_hl(0, 'CmpItemKindCopilot', { fg = '#6CC644' })
         end,
     },
     {
         'hrsh7th/nvim-cmp',
         event = 'InsertEnter',
         dependencies = {
-            -- Snippet Engine & its associated nvim-cmp source
-            {
-                'L3MON4D3/LuaSnip',
-                build = (function()
-                    -- Build Step is needed for regex support in snippets.
-                    -- This step is not supported in many windows environments.
-                    -- Remove the below condition to re-enable on windows.
-                    if vim.fn.has('win32') == 1 or vim.fn.executable('make') == 0 then
-                        return
-                    end
-                    return 'make install_jsregexp'
-                end)(),
-                dependencies = {
-                    -- `friendly-snippets` contains a variety of premade snippets.
-                    --    See the README about individual language/framework/plugin snippets:
-                    --    https://github.com/rafamadriz/friendly-snippets
-                    'rafamadriz/friendly-snippets',
-                },
-            },
-            'saadparwaiz1/cmp_luasnip',
-
-            -- Adds other completion capabilities.
-            --  nvim-cmp does not ship with all sources by default. They are split
-            --  into multiple repos for maintenance purposes.
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-path',
-
-            -- VSCode-like pictograms
-            {
-                'onsails/lspkind.nvim',
-                config = function()
-                    -- lspkind.lua
-                    local lspkind = require('lspkind')
-                    lspkind.init({
-                        symbol_map = {
-                            Copilot = '',
-                        },
-                    })
-                    vim.api.nvim_set_hl(0, 'CmpItemKindCopilot', { fg = '#6CC644' })
-                end,
-            },
+            -- Snippet Engine
+            'L3MON4D3/LuaSnip',
+            -- nvim-cmp sources
+            'saadparwaiz1/cmp_luasnip', -- LuaSnip
+            'hrsh7th/cmp-nvim-lsp', -- nvim-lsp
+            'hrsh7th/cmp-path', -- file/folder paths
+            'hrsh7th/cmp-nvim-lsp-signature-help', -- signature help
+            -- Visual enhancements
+            'onsails/lspkind.nvim',
         },
         config = function()
-            -- See `:help cmp`
             local cmp = require('cmp')
+            local lspkind = require('lspkind')
             local luasnip = require('luasnip')
-            luasnip.config.setup({})
 
             require('luasnip.loaders.from_vscode').lazy_load()
             require('luasnip').filetype_extend('cpp', { 'unreal', 'cppdoc' })
@@ -85,40 +77,19 @@ return {
                         luasnip.lsp_expand(args.body)
                     end,
                 },
-                completion = { completeopt = 'menu,menuone,noinsert' },
-
-                -- For an understanding of why these mappings were
-                -- chosen, you will need to read `:help ins-completion`
-                --
-                -- No, but seriously. Please read `:help ins-completion`, it is really good!
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                -- NOTE: See `:help ins-completion` to und understand why these kyemaps are chosen.
                 mapping = cmp.mapping.preset.insert({
-                    -- Select the [n]ext item
-                    ['<C-n>'] = cmp.mapping.select_next_item(),
-                    -- Select the [p]revious item
-                    ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-                    -- Scroll the documentation window [b]ack / [f]orward
-                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-
-                    -- Accept ([y]es) the completion.
-                    --  This will auto-import if your LSP supports it.
-                    --  This will expand snippets if the LSP sent a snippet.
+                    ['<C-n>'] = cmp.mapping.select_next_item(), -- Select the [n]ext item
+                    ['<C-p>'] = cmp.mapping.select_prev_item(), -- Select the [p]revious item
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Scrolls documentation [u]p
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Scrolls documentation [d]own
                     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-
-                    -- Manually trigger a completion from nvim-cmp.
-                    --  Generally you don't need this, because nvim-cmp will display
-                    --  completions whenever it has completion options available.
-                    ['<C-Space>'] = cmp.mapping.complete({}),
-
-                    -- Think of <c-l> as moving to the right of your snippet expansion.
-                    --  So if you have a snippet that's like:
-                    --  function $name($args)
-                    --    $body
-                    --  end
-                    --
-                    -- <c-.> will move you to the right of each of the expansion locations.
-                    -- <c-,> is similar, except moving you backwards.
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<C-Space>'] = cmp.mapping.complete({}), -- Manually trigger a completion from nvim-cmp.
                     ['<C-f>'] = cmp.mapping(function()
                         if luasnip.expand_or_locally_jumpable() then
                             luasnip.expand_or_jump()
@@ -129,23 +100,34 @@ return {
                             luasnip.jump(-1)
                         end
                     end, { 'i', 's' }),
-
                     -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
                     --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
                 }),
                 sources = {
-                    { name = 'copilot', group_index = 2 },
+                    -- group_index is used to prioritize sources, lower index = higher priority
+                    { name = 'copilot' },
                     {
                         name = 'nvim_lsp',
-                        group_index = 2,
                         option = {
+                            -- markdown_oxide is as lsp server for markdown (see lua/plugins/lsp.lua)
                             markdown_oxide = {
                                 keyword_pattern = [[\(\k\| \|\/\|#\)\+]],
                             },
                         },
                     },
-                    { name = 'luasnip', group_index = 2 },
-                    { name = 'path', group_index = 2 },
+                    { name = 'luasnip' },
+                    { name = 'path' },
+                    { name = 'nvim_lsp_signature_helper' },
+                },
+                ---@diagnostic disable-next-line: missing-fields
+                formatting = {
+                    format = lspkind.cmp_format({
+                        mode = 'symbol_text', -- Symbol first, then text
+                        maxwidth = 50,
+                        ellipsis_char = '…',
+                        -- Don't know exactly what the following option do exactly.
+                        -- show_menu_labelDetails = true, -- show labelDetails in menu. Disabled by default
+                    }),
                 },
             })
         end,
