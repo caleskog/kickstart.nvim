e15476ffa3f2e9475a5bba41cc97b68820b2a377
caleskog <christoffer.aleskog@gmail.com>
2024-12-22 12:50:06 +0100

    [pref] disabled cmp_premake call

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,3 +1,3 @@
 --- Set up Premake autocompletion
-local cmp_premake = require('cmp/sources/premake5')
-cmp_premake.setup()
+-- local cmp_premake = require('cmp/sources/premake5')
+-- cmp_premake.setup()
