ff1c7ff234b2515fd922e93430bf7af288d6465e
caleskog <christoffer.aleskog@gmail.com>
2024-12-25 17:11:23 +0100

    [add] `snacks.nvim` added

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -52,6 +52,25 @@ return {
             -- vim.cmd.hi('Comment gui=none')
         end,
     },
+    {
+        'folke/snacks.nvim',
+        priority = 1000,
+        lazy = false,
+        opts = {
+            toggle = {
+                enabled = true,
+            },
+        },
+        config = function(_, opts)
+            local notify = vim.notify
+            require('snacks').setup(opts)
+            -- HACK: restore vim.notify after snacks setup and let noice.nvim take over
+            -- this is needed to have early notifications show up in noice history
+            if Core.has('noice.nvim') then
+                vim.notify = notify
+            end
+        end,
+    },
 
     -- Highlight todo, notes, etc in comments
     {
--- lua/plugins/ui/context.lua
+++ lua/plugins/ui/context.lua
@@ -2,19 +2,19 @@ return {
     'nvim-treesitter/nvim-treesitter-context',
     enable = false,
     event = 'LazyFile',
-    -- opts = function()
-    --   local tsc = require("treesitter-context")
-    --   Snacks.toggle({
-    --     name = "Treesitter Context",
-    --     get = tsc.enabled,
-    --     set = function(state)
-    --       if state then
-    --         tsc.enable()
-    --       else
-    --         tsc.disable()
-    --       end
-    --     end,
-    --   }):map("<leader>ut")
-    --   return { mode = "cursor", max_lines = 3 }
-    -- end,
+    opts = function()
+        local tsc = require('treesitter-context')
+        Snacks.toggle({
+            name = 'Treesitter Context',
+            get = tsc.enabled,
+            set = function(state)
+                if state then
+                    tsc.enable()
+                else
+                    tsc.disable()
+                end
+            end,
+        }):map('<leader>ut')
+        return { mode = 'cursor', max_lines = 3 }
+    end,
 }
