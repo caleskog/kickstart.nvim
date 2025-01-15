eda492d093724934d7a8d366c03404717fb85a02
caleskog <christoffer.aleskog@gmail.com>
2025-01-15 20:58:05 +0100

    [fix] surround now works

--- lua/plugins/coding/surround.lua
+++ lua/plugins/coding/surround.lua
@@ -1,32 +1,42 @@
 -- Author: caleskog
 
+-- Fast and feature-rich surround actions. For text that includes
+-- surrounding characters like brackets or quotes, this allows you
+-- to select the text inside, change or modify the surrounding characters,
+-- and more.
 return {
     {
         'echasnovski/mini.surround',
-        version = '*',
+        -- version = '*',
+        recommended = true,
+        keys = function(_, keys)
+            -- Populate the keys based on the user's options
+            local opts = Core.opts('mini.surround')
+            local mappings = {
+                { opts.mappings.add, desc = 'Add Surrounding', mode = { 'n', 'v' } },
+                { opts.mappings.delete, desc = 'Delete Surrounding' },
+                { opts.mappings.find, desc = 'Find Right Surrounding' },
+                { opts.mappings.find_left, desc = 'Find Left Surrounding' },
+                { opts.mappings.highlight, desc = 'Highlight Surrounding' },
+                { opts.mappings.replace, desc = 'Replace Surrounding' },
+                { opts.mappings.update_n_lines, desc = 'Update `MiniSurround.config.n_lines`' },
+                { 'gs', '', desc = '+surround' },
+            }
+            mappings = vim.tbl_filter(function(m)
+                return m[1] and #m[1] > 0
+            end, mappings)
+            return vim.list_extend(mappings, keys)
+        end,
         opts = {
             mappings = {
-                add = 'gsa',
-                delete = 'gsd',
-                find = 'gsf',
-                find_left = 'gsF',
-                highlight = 'gsh',
-                replace = 'gsr',
-                update = 'gsu',
-                suffix_last = 'gs,',
-                suffix_next = 'gs;',
+                add = 'gsa', -- Add surrounding in Normal and Visual modes
+                delete = 'gsd', -- Delete surrounding
+                find = 'gsf', -- Find surrounding (to the right)
+                find_left = 'gsF', -- Find surrounding (to the left)
+                highlight = 'gsh', -- Highlight surrounding
+                replace = 'gsr', -- Replace surrounding
+                update_n_lines = 'gsn', -- Update `n_lines`
             },
-        },
-        config = function(opts)
-            -- Add/delete/replace surroundings (brackets, quotes, etc.)
-            --
-            -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-            -- - sd'   - [S]urround [D]elete [']quotes
-            -- - sr)'  - [S]urround [R]eplace [)] [']
-            require('mini.surround').setup({ opts })
-        end,
-        keys = {
-            { 'gs', '', desc = '+surround' },
         },
     },
 }
